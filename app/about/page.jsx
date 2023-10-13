7f3cdd4ac300ec4300a71957e153f6a5dc8b86ac
vishalkrsharma <vishalkrsharma1234@gmail.com>
2023-10-14 01:32:12 +0530

    quote escaped

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -18,8 +18,8 @@ const Page = () => {
         When we started working on this project, it was just for fun. As we dived deep into it, we realized how little we know compared to what we thought. Our
         aim is to share whatever we have learned so far with everyone. We want to make everybody aware about the people who went to moon, did not went just for
         fun, but for some scientific purposes too, that the space which looks quiet is not a calm place. What might look calm to our eyes might be very chaotic
-        in reality. The Moon, our Earth's true companion has it's own secrets. Hiding in it's belly the moonquakes which makes it ring like a bell. Alas! We
-        thought there is no sound on the moon. Let's explore this fascinating place on our app 'Moonquake Visualized'.
+        in reality. The Moon, our Earth&apos;s true companion has it&apos;s own secrets. Hiding in it&apos;s belly the moonquakes which makes it ring like a
+        bell. Alas! We thought there is no sound on the moon. Let&apos;s explore this fascinating place on our app &apos;Moonquake Visualized&apos;.
       </div>
       <Developers />
     </div>
